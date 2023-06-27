04829753a7b6ac6a087fc3c9ec1160fd4b9d3579
denisyordanp <denis.yordan.p@gmail.com>
2023-06-27 15:26:10 +0700

    update main repository

--- app/src/main/java/com/denisyordanp/mymoviecatalogue/repositories/MainRepository.kt
+++ app/src/main/java/com/denisyordanp/mymoviecatalogue/repositories/MainRepository.kt
@@ -1,9 +1,12 @@
 package com.denisyordanp.mymoviecatalogue.repositories
 
 import com.denisyordanp.mymoviecatalogue.schemas.ui.Genre
+import com.denisyordanp.mymoviecatalogue.schemas.ui.Movie
 import kotlinx.coroutines.flow.Flow
 
 interface MainRepository {
     suspend fun reloadGenres()
     fun getGenres(): Flow<List<Genre>>
+    suspend fun reloadMovies(genreId: Int)
+    fun getMovies(genreId: Int): Flow<List<Movie>>
 }
--- app/src/main/java/com/denisyordanp/mymoviecatalogue/repositories/impl/MainRepositoryImpl.kt
+++ app/src/main/java/com/denisyordanp/mymoviecatalogue/repositories/impl/MainRepositoryImpl.kt
@@ -1,26 +1,42 @@
 package com.denisyordanp.mymoviecatalogue.repositories.impl
 
 import com.denisyordanp.mymoviecatalogue.database.GenresDao
+import com.denisyordanp.mymoviecatalogue.database.MoviesDao
 import com.denisyordanp.mymoviecatalogue.network.MovieService
 import com.denisyordanp.mymoviecatalogue.repositories.MainRepository
 import com.denisyordanp.mymoviecatalogue.schemas.ui.Genre
+import com.denisyordanp.mymoviecatalogue.schemas.ui.Movie
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class MainRepositoryImpl @Inject constructor(
     private val service: MovieService,
-    private val dao: GenresDao
+    private val genreDao: GenresDao,
+    private val movieDao: MoviesDao
 ) : MainRepository {
     override suspend fun reloadGenres() {
         val genres = service.fetchGenres().toEntities()
-        dao.insertGenres(genres)
+        genreDao.insertGenres(genres)
     }
 
     override fun getGenres(): Flow<List<Genre>> {
-        return dao.getGenres().map { genres ->
+        return genreDao.getGenres().map { genres ->
             genres.map { genre ->
                 genre.toUi()
+            }
+        }
+    }
+
+    override suspend fun reloadMovies(genreId: Int) {
+        val movies = service.fetchMovies(genre = genreId).toEntities(genreId)
+        movieDao.insertMovies(movies)
+    }
+
+    override fun getMovies(genreId: Int): Flow<List<Movie>> {
+        return movieDao.getMovies(genreId).map { movies ->
+            movies.map { movie ->
+                movie.toUi()
             }
         }
     }
