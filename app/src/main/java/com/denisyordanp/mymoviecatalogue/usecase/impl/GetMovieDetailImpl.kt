67d6bc63c18770521e05a8995df991f19bd5e663
Denis Y Panggabean <denis.yordan.p@gmail.com>
2023-08-08 09:05:47 +0700

    add getMovieDetail tests

--- app/src/main/java/com/denisyordanp/mymoviecatalogue/usecase/impl/GetMovieDetailImpl.kt
+++ app/src/main/java/com/denisyordanp/mymoviecatalogue/usecase/impl/GetMovieDetailImpl.kt
@@ -3,16 +3,17 @@ package com.denisyordanp.mymoviecatalogue.usecase.impl
 import com.denisyordanp.mymoviecatalogue.repositories.MovieDetailRepository
 import com.denisyordanp.mymoviecatalogue.usecase.GetMovieDetail
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class GetMovieDetailImpl @Inject constructor(
     private val repository: MovieDetailRepository
 ) : GetMovieDetail {
     override fun invoke(movieId: Long, isForce: Boolean) = flow {
-        if (isForce) {
-            repository.reloadMovieDetail(movieId)
-        }
         repository.getMovieDetail(movieId)
+            .onStart {
+                if (isForce) repository.reloadMovieDetail(movieId)
+            }
             .collect {
                 if (it == null) {
                     repository.reloadMovieDetail(movieId)
---
+++ app/src/test/java/com/denisyordanp/mymoviecatalogue/usecase/impl/GetMovieDetailImplTest.kt
@@ -0,0 +1,149 @@
+package com.denisyordanp.mymoviecatalogue.usecase.impl
+
+import com.denisyordanp.mymoviecatalogue.helper.Dummy
+import com.denisyordanp.mymoviecatalogue.repositories.MovieDetailRepository
+import com.denisyordanp.mymoviecatalogue.usecase.GetMovieDetail
+import com.nhaarman.mockitokotlin2.MockitoKotlinException
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import kotlin.random.Random
+
+internal class GetMovieDetailImplTest {
+    private lateinit var underTest: GetMovieDetail
+    private val mockedMovieDetailRepository = mock<MovieDetailRepository>()
+
+    @Before
+    fun setup() {
+        underTest = GetMovieDetailImpl(mockedMovieDetailRepository)
+    }
+
+    @Test
+    fun `invoke with isForce true should call reloadMovieDetail and return expected data`() =
+        runTest {
+            val isForce = true
+            val dummyId = Random.nextLong()
+            val dummyGenres = Dummy.getDbGenres()
+            val expected = Dummy.getDbMovieDetail().toUi(dummyGenres)
+
+            whenever(mockedMovieDetailRepository.getMovieDetail(dummyId)).thenReturn(flowOf(expected))
+
+            val actual = underTest(dummyId, isForce).first()
+
+            Assert.assertEquals(expected, actual)
+
+            verify(mockedMovieDetailRepository).reloadMovieDetail(dummyId)
+        }
+
+    @Test
+    fun `invoke with isForce false should not call reloadMovieDetail and return expected data`() =
+        runTest {
+            val isForce = false
+            val dummyId = Random.nextLong()
+            val dummyGenres = Dummy.getDbGenres()
+            val expected = Dummy.getDbMovieDetail().toUi(dummyGenres)
+
+            whenever(mockedMovieDetailRepository.getMovieDetail(dummyId)).thenReturn(flowOf(expected))
+
+            val actual = underTest(dummyId, isForce).first()
+
+            Assert.assertEquals(expected, actual)
+
+            verify(mockedMovieDetailRepository, never()).reloadMovieDetail(dummyId)
+        }
+
+    @Test
+    fun `invoke should call reloadMovieDetail when data is null`() = runTest {
+        val isForce = false
+        val dummyId = Random.nextLong()
+        val dummyGenres = Dummy.getDbGenres()
+        val expected = Dummy.getDbMovieDetail().toUi(dummyGenres)
+
+        whenever(mockedMovieDetailRepository.getMovieDetail(dummyId))
+            .thenReturn(flow {
+                emit(null)
+                emit(expected)
+            })
+
+        val actual = underTest(dummyId, isForce).last()
+
+        Assert.assertEquals(expected, actual)
+
+        verify(mockedMovieDetailRepository).reloadMovieDetail(dummyId)
+    }
+
+    @Test
+    fun `invoke should not call reloadMovieDetail when data is not null`() = runTest {
+        val isForce = false
+        val dummyId = Random.nextLong()
+        val dummyGenres = Dummy.getDbGenres()
+        val expected = Dummy.getDbMovieDetail().toUi(dummyGenres)
+
+        whenever(mockedMovieDetailRepository.getMovieDetail(dummyId))
+            .thenReturn(flowOf(expected))
+
+        val actual = underTest(dummyId, isForce).last()
+
+        Assert.assertEquals(expected, actual)
+
+        verify(mockedMovieDetailRepository, never()).reloadMovieDetail(dummyId)
+    }
+
+    @Test
+    fun `invoke should throw error when getMovieDetail error`() = runTest {
+        val isForce = true
+        val dummyId = Random.nextLong()
+        val expected = MockitoKotlinException(null, null)
+
+        whenever(mockedMovieDetailRepository.getMovieDetail(dummyId)).thenThrow(expected)
+
+        try {
+            underTest(dummyId, isForce)
+        } catch (actual: Exception) {
+            Assert.assertTrue(actual is MockitoKotlinException)
+            Assert.assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `invoke with isForce true should throw error when reloadMovieDetail error`() = runTest {
+        val isForce = true
+        val dummyId = Random.nextLong()
+        val expected = MockitoKotlinException(null, null)
+
+        whenever(mockedMovieDetailRepository.reloadMovieDetail(dummyId)).thenThrow(expected)
+
+        try {
+            underTest(dummyId, isForce)
+        } catch (actual: Exception) {
+            Assert.assertTrue(actual is MockitoKotlinException)
+            Assert.assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `invoke should throw error when data is null and getMovieDetail error`() = runTest {
+        val isForce = false
+        val dummyId = Random.nextLong()
+        val expected = MockitoKotlinException(null, null)
+
+        whenever(mockedMovieDetailRepository.getMovieDetail(dummyId)).thenReturn(flowOf(null))
+        whenever(mockedMovieDetailRepository.reloadMovieDetail(dummyId)).thenThrow(expected)
+
+        try {
+            underTest(dummyId, isForce)
+        } catch (actual: Exception) {
+            Assert.assertTrue(actual is MockitoKotlinException)
+            Assert.assertEquals(expected, actual)
+        }
+    }
+}
