17da065babcbcbada2408b425a5a7161c7fa0446
Denis Y Panggabean <denis.yordan.p@gmail.com>
2023-08-08 21:08:21 +0700

    fix movie detail always fetched

--- app/src/main/java/com/denisyordanp/mymoviecatalogue/usecase/GetMovieDetail.kt
+++ app/src/main/java/com/denisyordanp/mymoviecatalogue/usecase/GetMovieDetail.kt
@@ -4,5 +4,5 @@ import com.denisyordanp.mymoviecatalogue.schemas.ui.MovieDetail
 import kotlinx.coroutines.flow.Flow
 
 fun interface GetMovieDetail {
-    operator fun invoke(movieId: Long, isForce: Boolean): Flow<MovieDetail>
+    operator fun invoke(movieId: Long, isForce: Boolean): Flow<MovieDetail?>
 }
--- app/src/main/java/com/denisyordanp/mymoviecatalogue/usecase/impl/GetMovieDetailImpl.kt
+++ app/src/main/java/com/denisyordanp/mymoviecatalogue/usecase/impl/GetMovieDetailImpl.kt
@@ -2,24 +2,21 @@ package com.denisyordanp.mymoviecatalogue.usecase.impl
 
 import com.denisyordanp.mymoviecatalogue.repositories.MovieDetailRepository
 import com.denisyordanp.mymoviecatalogue.usecase.GetMovieDetail
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class GetMovieDetailImpl @Inject constructor(
-    private val repository: MovieDetailRepository
+    private val repository: MovieDetailRepository,
 ) : GetMovieDetail {
     override fun invoke(movieId: Long, isForce: Boolean) = flow {
-        repository.getMovieDetail(movieId)
-            .onStart {
-                if (isForce) repository.reloadMovieDetail(movieId)
-            }
-            .collect {
-                if (it == null) {
-                    repository.reloadMovieDetail(movieId)
-                } else {
-                    emit(it)
-                }
-            }
+        val detailFlow = repository.getMovieDetail(movieId)
+        val initial = detailFlow.first()
+
+        if (initial == null || isForce) {
+            repository.reloadMovieDetail(movieId)
+        }
+
+        detailFlow.collect(this)
     }
 }
